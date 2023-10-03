556499893eac556523e6a593e922656f0866a9b5
Chvalov K. R <71774684+KASSTUSS@users.noreply.github.com>
2023-10-03 13:14:58 +0300

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 3001;
 
 const app = express();
 
-// const cors = require("cors");
+const cors = require("cors");
 // app.use(
 //   cors({
 //     origin: "https://student-gsu.netlify.app",
